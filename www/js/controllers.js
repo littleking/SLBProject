875a90019084389a37b1795e9b58115e3fefd96f
Ryan Jin <gjin003@gmail.com>
2015-09-21 22:57:53 +0800

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jg on 2015-09-21.
+ */
+angular.module('starter.controllers', [])
+
+  .controller('OverviewCtrl', function($scope) {})
+
+  .controller('DispatchCtrl', function($scope) {})
+
+  .controller('ProductCtrl', function($scope) {})
+
+  .controller('WarehouseCtrl', function($scope) {});
